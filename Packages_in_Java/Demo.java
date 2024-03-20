f165c8a6981920871f497698d9a85f65675fc186
RankracerBR <augustopontes010@gmail.com>
2024-03-19 22:15:31 -0300

    jv learning

---
+++ Packages_in_Java/Demo.java
@@ -0,0 +1,7 @@
+package Packages_in_Java;
+
+public class Demo {
+    public void msg(){
+        System.out.println("Welcome");
+    }
+}
---
+++ Packages_in_Java/Demo1.java
@@ -0,0 +1,9 @@
+package Packages_in_Java;
+import Demo;
+
+public class Demo1 {
+    public static void main(String []args){
+        Demo obj = new Demo();
+        obj.msg();
+    }
+}
